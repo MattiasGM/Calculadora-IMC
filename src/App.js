8e4a852918780365221586831bff9f1fc49b2198
Mattias Gaspar <mattiasmac2000@gmail.com>
2022-06-15 20:04:38 -0300

    finish

---
+++ src/App.js
@@ -0,0 +1,29 @@
+function App() {
+    formButton.addEventListener('click', submit)
+
+    function submit() {
+        let peso = document.querySelector('#formPeso').value.replace(',','.')
+        let altura = document.querySelector('#formAltura').value.replace(',','.')
+        let divResults = document.querySelector('#results')
+        let imc = peso / altura ** 2
+
+        if(peso.length !== 0 && altura.length !== 0 && imc) { //verificação de inputs
+            divResults.innerHTML = `Seu IMC é ${imc.toFixed(2)} (${getDiagnosticImc(imc)})`
+            divResults.setAttribute('class', 'correct')
+        }else { 
+            divResults.innerHTML = `Preencha todos os campos corretamente!`
+            divResults.setAttribute('class', 'error')
+        }
+    }
+
+    function getDiagnosticImc(imc) {
+        const grau = ['Obesidade grau III', 'Obesidade grau II', 'Obesidade grau I', 'Sobrepeso', 'Normal', 'Abaixo do peso', 'alien']
+        const nivel = [39.9, 34.9, 29.9, 24.9, 18.5]
+
+        for(let pos in nivel) {
+            return imc >= nivel[pos] ? grau[6] : grau[pos]
+        }
+    }
+}
+
+App()
